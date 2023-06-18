bfe9cc38768fb592c6b4bca1aed3490ed0852690
Siwa <siwarjsa@gmail.com>
2023-06-18 19:11:49 +0800

    update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,75 @@
+@extends('template')
+@section('main')
+    <section id="about">
+        <div class="section-container">
+            <h2 class="section-title">About Me</h2>
+            <p class="section-subtitle">My Introduction</p>
+            <div class="about-image">
+                <img class="blob-image" src="assets/blob-orange.png" alt="">
+                <img class="blogger-image" src="assets/Normal 1.png" alt="">
+            </div>
+            <div>
+                <p>I am Cobee, 16 years old student who lives in Denpasar.
+                    Started to learn coding when I was an elementary student.
+                    I was learning Scratch and create a simple Memory Game with my own code. From
+                    that day, I become a fan of coding and started to learn more, for example
+                    Javascript Game using Phaser Framework and also Web Development.</p>
+                <h4>Cobee</h4>
+            </div>
+        </div>
+
+        </div>
+    </section>
+
+    <section id="skill">
+        <div class="section-container">
+            <h2 class="section-title">Skills</h2>
+            <p class="section-subtitle">My Technical Skill for 3 years Learn Coding</p>
+            <div class="skills-flex">
+                <div class="skill-item">
+                    <img src="assets/scratch.png" alt="">
+                    <h4>Game Development with Scratch</h4>
+                    <p>1 year</p>
+                </div>
+                <div class="skill-item">
+                    <img src="assets/phaser.png" alt="">
+                    <h4>Game Development with Phaser</h4>
+                    <p>2 years</p>
+                </div>
+                <div class="skill-item">
+                    <img src="assets/html.png" alt="">
+                    <h4>Website Development</h4>
+                    <p>1 year</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section id="activity">
+        <div class="section-container">
+            <h2 class="section-title">Activity</h2>
+            <p class="section-subtitle">My Activity</p>
+            <div class="activity-flex">
+                <div class="activity-item">
+                    <img src="assets/activity1.jpg" alt="">
+                </div>
+                <div class="activity-item">
+                    <img src="assets/activity2.jpg" alt="">
+                </div>
+                <div class="activity-item">
+                    <img src="assets/activity3.jpg" alt="">
+                </div>
+                <div class="activity-item">
+                    <img src="assets/activity4.jpg" alt="">
+                </div>
+                <div class="activity-item">
+                    <img src="assets/activity5.jpg" alt="">
+                </div>
+                <div class="activity-item">
+                    <img src="assets/activity6.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href={{ asset('assets/css/style.css') }} />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <script src="https://kit.fontawesome.com/f90068558c.js" crossorigin="anonymous"></script>
+
+    <title>E-Shop</title>
+</head>
+
+<body style="background-image: url(assets/bg_bggenerator_com.png);background-size: cover;">
+    <div class="splash">
+        <h1 class="splash-text" data-aos="fade-right" data-aos-duration="200">
+            Hello Guys! <br />
+            Welcome to our shop!
+        </h1>
+        <img class="astronaut-dabbing" src="assets/astronaut1.png" alt="" />
+        <img class="astronaut-takeoff" src="assets/astronaut2.png" alt="" />
+    </div>
+
+    <header>
+        <div class="header">
+            <img class="header-icon" src="assets/header-icon.png" alt="" />
+            <h3 class="title">
+                <a href="/">E-Shop</a>
+            </h3>
+            <nav>
+                <a href="/product">Product</a>
+                <a href="/contact">Contact</a>
+                <a href="/about">About Us</a>
+            </nav>
+        </div>
+    </header>
+    <main>
+        @yield('main')
+    </main>
+    <footer>
+        <div class="main-footer">
+            <h3 class="info-top">E-shop</h3>
+            <div class="social-media">
+                <i class="fab fa-instagram-square"></i>
+                <i class="fab fa-twitter-square"></i>
+                <i class="fab fa-facebook-square"></i>
+                <i class="fas fa-envelope"></i>
+            </div>
+            <p class="info-center">Build with ❤ by Kelompok 4</p>
+        </div>
+    </footer>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src={{ asset('assets/js/script.js') }}></script>
+</body>
+
+</html>
