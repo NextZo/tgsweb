ba0f33e78ff0f15b23faf7d28f96e47ae6ca8a44
Siwa <siwarjsa@gmail.com>
2023-07-11 20:30:01 +0800

    update

--- resources/views/troli.blade.php
+++ resources/views/troli.blade.php
@@ -52,9 +52,17 @@
             </div>
         </div>
 
-        <button
-            class="text-center border-2 w-full my-4 py-2 rounded-lg text-blue-600 border-blue-600 hover:bg-blue-600 hover:text-white transition"
-            data-bs-toggle="modal" data-bs-target="#checkout">Checkout</button>
+        @if ($collection->isNotEmpty())
+            <button
+                class="text-center border-2 w-full my-4 py-2 rounded-lg text-blue-600 border-blue-600 hover:bg-blue-600 hover:text-white transition"
+                data-bs-toggle="modal" data-bs-target="#checkout">Checkout</button>
+        @else
+            <div>
+                <h1 class="text-2xl font-bold">Keranjang masih kosong nih</h1>
+                <a href="/kategori" class="text-blue-600 underline">Mulai belanja</a>
+            </div>
+        @endif
+
 
         <script>
             const storage = {
