20916fab67e75bba328a8bef18070cc3f9feed54
NextZo <fonzolydio@gmail.com>
2023-07-11 19:29:37 +0700

    update

--- resources/views/categories/kemeja.blade.php
+++ resources/views/categories/kemeja.blade.php
@@ -14,39 +14,39 @@
                     <h3 class="text-xl font-normal">{{ $kemeja->nama }}</h3>
                     <p>Rp{{ $kemeja->harga }}</p>
                     <div class="absolute right-0 top-0 bottom-0 flex flex-col justify-center px-6 py-10 gap-3">
-                        @if ($kaos->s)
-                            <button onclick="size('s-{{ $kaos->id }}')"
+                        @if ($kemeja->s)
+                            <button onclick="size('s-{{ $kemeja->id }}')"
                                 class="border-2 border-blue-600 py-2 px-4 rounded text-blue-600 active:bg-blue-600 active:text-slate-100 transition uppercase">s</button>
                         @else
                             <button
                                 class="border-2 border-blue-800 py-2 px-4 rounded bg-blue-800 text-slate-200 cursor-not-allowed uppercase">s</button>
                         @endif
 
-                        @if ($kaos->m)
-                            <button onclick="size('m-{{ $kaos->id }}')"
+                        @if ($kemeja->m)
+                            <button onclick="size('m-{{ $kemeja->id }}')"
                                 class="border-2 border-blue-600 py-2 px-4 rounded text-blue-600 active:bg-blue-600 active:text-slate-100 transition uppercase">m</button>
                         @else
                             <button
                                 class="border-2 border-blue-800 py-2 px-4 rounded bg-blue-800 text-slate-200 cursor-not-allowed uppercase">m</button>
                         @endif
 
-                        @if ($kaos->l)
-                            <button onclick="size('l-{{ $kaos->id }}')"
+                        @if ($kemeja->l)
+                            <button onclick="size('l-{{ $kemeja->id }}')"
                                 class="border-2 border-blue-600 py-2 px-4 rounded text-blue-600 active:bg-blue-600 active:text-slate-100 transition uppercase">l</button>
                         @else
                             <button
                                 class="border-2 border-blue-800 py-2 px-4 rounded bg-blue-800 text-slate-200 cursor-not-allowed uppercase">l</button>
                         @endif
 
-                        @if ($kaos->xl)
-                            <button onclick="size('xl-{{ $kaos->id }}')"
+                        @if ($kemeja->xl)
+                            <button onclick="size('xl-{{ $kemeja->id }}')"
                                 class="border-2 border-blue-600 py-2 px-4 rounded text-blue-600 active:bg-blue-600 active:text-slate-100 transition uppercase">xl</button>
                         @else
                             <button
                                 class="border-2 border-blue-800 py-2 px-4 rounded bg-blue-800 text-slate-200 cursor-not-allowed uppercase">xl</button>
                         @endif
                     </div>
-                    <button onclick="addToKeranjang({{ $kaos->id }})"
+                    <button onclick="addToKeranjang({{ $kemeja->id }})"
                         class="border-2 border-transparent rounded-lg bg-blue-600 text-white fill-white py-2 px-4 flex items-center gap-1 absolute bottom-5 hover:fill-blue-600 hover:text-blue-600 hover:border-blue-600 hover:bg-white transition">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
